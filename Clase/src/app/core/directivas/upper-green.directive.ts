25fa7f10ee2c3ccf6d516f88734f107d2cdea1a7
Antonio Contreras Cardenas <aconcar181@g.educaand.es>
2024-10-23 14:04:16 +0200

    Modificacion Directiva

--- Clase/src/app/core/directivas/upper-green.directive.ts
+++ Clase/src/app/core/directivas/upper-green.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[upperGreen]',
@@ -9,18 +15,23 @@ export class UpperGreenDirective {
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  @HostBinding('class') currentClass = '';
+
   @HostListener('mouseenter') onMouseEnter() {
     this.originalText = this.el.nativeElement.innerText;
-    this.renderer.addClass(this.el.nativeElement, 'upperborder');
+    //this.renderer.addClass(this.el.nativeElement, 'upperborder');
     this.renderer.setProperty(
       this.el.nativeElement,
       'innerText',
       this.originalText.toUpperCase()
     );
+
+    this.currentClass = 'upperborder';
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.renderer.removeClass(this.el.nativeElement, 'upperborder');
+    //this.renderer.removeClass(this.el.nativeElement, 'upperborder');
+    this.currentClass = '';
     this.renderer.setProperty(
       this.el.nativeElement,
       'innerText',
