977b8291932b6d28fdfd346ba449274e87c95439
Antonio Contreras Cardenas <aconcar181@g.educaand.es>
2024-10-23 09:25:02 +0200

    Ejercicios terminados

--- Clase/src/app/app.component.ts
+++ Clase/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PersonasComponent } from './personas/personas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CabeceraComponent],
+  imports: [RouterOutlet, CabeceraComponent, PersonasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ Clase/src/app/personas/personas.component.ts
@@ -0,0 +1,87 @@
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+
+interface Direccion {
+  calle: string;
+  numero: number;
+  ciudad: string;
+  pais: string;
+}
+
+interface Persona {
+  nombre: string;
+  edad: number;
+  direccion: Direccion;
+  hobbies: string[];
+  activo: boolean;
+}
+
+@Component({
+  selector: 'personas',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './personas.component.html',
+  styleUrl: './personas.component.scss',
+})
+export class PersonasComponent {
+  public hidden: boolean = false;
+  public classAplicated: boolean = false;
+
+  public personas: Persona[] = [
+    {
+      nombre: 'Juan',
+      edad: 30,
+      direccion: {
+        calle: 'Calle Mayor',
+        numero: 10,
+        ciudad: 'Madrid',
+        pais: 'España',
+      },
+      hobbies: ['Leer', 'Correr', 'Cocinar'],
+      activo: true,
+    },
+    {
+      nombre: 'Ana',
+      edad: 25,
+      direccion: {
+        calle: 'Avenida de la Constitución',
+        numero: 20,
+        ciudad: 'Sevilla',
+        pais: 'España',
+      },
+      hobbies: ['Bailar', 'Viajar'],
+      activo: false,
+    },
+    {
+      nombre: 'Pedro',
+      edad: 40,
+      direccion: {
+        calle: 'Plaza del Sol',
+        numero: 5,
+        ciudad: 'Barcelona',
+        pais: 'España',
+      },
+      hobbies: ['Pintar', 'Pescar', 'Jardinería'],
+      activo: true,
+    },
+    {
+      nombre: 'María',
+      edad: 35,
+      direccion: {
+        calle: 'Calle de las Flores',
+        numero: 12,
+        ciudad: 'Valencia',
+        pais: 'España',
+      },
+      hobbies: ['Fotografía', 'Escribir'],
+      activo: true,
+    },
+  ];
+
+  public toggleBtnHidde(): void {
+    this.hidden = !this.hidden;
+  }
+  public toggleBtnClass(): void {
+    this.classAplicated = !this.classAplicated;
+  }
+}
