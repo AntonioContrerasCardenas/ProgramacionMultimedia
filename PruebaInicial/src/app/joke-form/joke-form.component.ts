69f989dcd45025f4dd6cf42fedec1ac7c120010f
Antonio Contreras Cardenas <aconcar181@g.educaand.es>
2024-10-29 19:14:40 +0100

    Ej formulario terminado

--- PruebaInicial/src/app/joke-form/joke-form.component.ts
+++ PruebaInicial/src/app/joke-form/joke-form.component.ts
@@ -7,12 +7,18 @@ import {
   Output,
 } from '@angular/core';
 import { JokeInterface } from '../core/interface/joke.interface';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'joke-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './joke-form.component.html',
   styleUrl: './joke-form.component.scss',
 })
@@ -27,6 +33,41 @@ export class JokeFormComponent {
     punchline: ['', [Validators.required, Validators.minLength(6)]],
   });
 
+  public getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return '';
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'El campo es requerido';
+
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracteres`;
+        default:
+          break;
+      }
+    }
+
+    return null;
+  }
+
+  public isInValidField(field: string): boolean | undefined {
+    const fieldControl = this.myForm.get(field);
+    return fieldControl?.touched && fieldControl?.invalid;
+  }
+
+  onSubmit(setup: string, punchline: string) {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    this.addJoke(setup, punchline);
+  }
+
+  //Version anterior
   jokeNew = output<JokeInterface>();
 
   addJoke(setup: string, punchline: string) {
