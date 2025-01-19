9ba4d72e28afaff72fde61a040d1a96da8c842b9
Antonio <aconcar181@g.educaand.es>
2025-01-19 18:50:08 +0100

    Mejoras ejercicio 2

--- Express_Angular/Ejercicio2/Frontend/src/app/questions/components/question-card/question-card.component.ts
+++ Express_Angular/Ejercicio2/Frontend/src/app/questions/components/question-card/question-card.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { Question } from '../../../core/interfaces/questions.interfaces';
 import { NgClass } from '@angular/common';
 
@@ -8,24 +14,36 @@ import { NgClass } from '@angular/common';
   imports: [NgClass],
   templateUrl: './question-card.component.html',
   styleUrl: './question-card.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuestionCardComponent {
+export class QuestionCardComponent implements OnChanges {
   @Input({ required: true })
   question: Question = {} as Question;
 
-  public optionSelected: string = '';
+  public optionsSelected: string[] = [];
+  public isCorrectAnswerSelected: boolean = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['question']) {
+      this.optionsSelected = [];
+      this.isCorrectAnswerSelected = false;
+    }
+  }
 
   public checkAnswer(option: string) {
-    this.optionSelected = option;
+    if (this.isCorrect(option)) {
+      this.isCorrectAnswerSelected = true;
+    }
+    if (!this.optionsSelected.includes(option)) {
+      this.optionsSelected.push(option);
+    }
   }
 
   public isCorrect(option: string): boolean {
-    if (option === this.question.answer) return true;
-    return false;
+    return option === this.question.answer;
   }
 
   public isSelected(option: string): boolean {
-    if (option === this.optionSelected) return true;
-    return false;
+    return this.optionsSelected.includes(option);
   }
 }
