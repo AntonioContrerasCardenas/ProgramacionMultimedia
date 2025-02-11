1894bc6db66438df90ff0078f75b4998dfa29f20
Antonio Contreras <aconcar181@g.educaand.es>
2025-02-11 11:50:16 +0100

    Enhance Question model to include userId and randomize options in JSON output

--- Express_Angular/Ejercicio2/Backend/src/models/Question.ts
+++ Express_Angular/Ejercicio2/Backend/src/models/Question.ts
@@ -8,20 +8,30 @@ export interface QuestionI extends Document {
   userId: Types.ObjectId
 }
 
-const QuestionSchema = new Schema<QuestionI>({
-  question: { type: String, required: true },
-  answer: { type: String, required: true },
-  options: { type: [String], required: true },
-  categoryId: {
-    type: Schema.Types.ObjectId,
-    ref: 'Category',
-    required: true,
+const QuestionSchema = new Schema<QuestionI>(
+  {
+    question: { type: String, required: true },
+    answer: { type: String, required: true },
+    options: { type: [String], required: true },
+    categoryId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-})
+  {
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.options = [...doc.options].sort(() => Math.random() - 0.5)
+        return ret
+      },
+    },
+  }
+)
 
 export const Question = model<QuestionI>('Question', QuestionSchema)
